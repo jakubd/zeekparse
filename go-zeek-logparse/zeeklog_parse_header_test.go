2f70549c51dbc2db29f46d69050e1db755c5a3f6
jakub <jakub.dalek@utoronto.ca>
2020-06-15 14:21:00 -0400

    switch to testify for assertions

--- go-zeek-logparse/zeeklog_parse_header_test.go
+++ go-zeek-logparse/zeeklog_parse_header_test.go
@@ -2,39 +2,22 @@ package go_zeek_logparse
 
 import (
 	"testing"
+	"github.com/stretchr/testify/assert"
 )
-
-func assertStringsEqual(t *testing.T, value, shouldbe string) {
-	if value != shouldbe {
-		t.Errorf("value should be %s not %s", shouldbe, value)
-	}
-}
-
-func assertNoError(t *testing.T, err error) {
-	if err != nil {
-		t.Errorf("error assert failed error thrown was: %s", err)
-	}
-}
-
-func asserErrorThrown(t *testing.T, err error, errMsgShouldBe string) {
-	if err.Error() != errMsgShouldBe {
-		t.Errorf("error mismatch this should be thrown: '%s' but this is: '%s'", errMsgShouldBe, err)
-	}
-}
 
 func zeekHeaderGoodCase(t *testing.T) {
 	zeekLogFn := "test_input/proper_header.log"
 	data, err := parseZeekLogHeader(zeekLogFn)
-	assertNoError(t, err)
+	assert.NoError(t, err)
 
 	if data.open.Year() == 0001 {
 		t.Errorf("date wasnt parsed out of open")
 	}
 
-	assertStringsEqual(t, data.setSeparator, ",")
-	assertStringsEqual(t, data.path, "dns")
-	assertStringsEqual(t, data.emptyField, "(empty)")
-	assertStringsEqual(t, data.unsetField, "-")
+	assert.Equal(t, data.setSeparator, ",")
+	assert.Equal(t, data.path, "dns")
+	assert.Equal(t, data.emptyField, "(empty)")
+	assert.Equal(t, data.unsetField, "-")
 
 	if len(data.fieldMapping) == 0 {
 		t.Errorf("fieldMapping was blank when parsing zeek log header")
@@ -44,19 +27,19 @@ func zeekHeaderGoodCase(t *testing.T) {
 func zeekHeaderFileDoesntExist(t *testing.T) {
 	broLogFn := "dndsfs.log"
 	_, err := parseZeekLogHeader(broLogFn)
-	asserErrorThrown(t, err, "open file error")
+	assert.Error(t, err, "open file error")
 }
 
 func zeekHeaderFieldsMismatched(t *testing.T) {
 	broLogFn :=  "test_input/mismatched_fields_header.log"
 	_, err := parseZeekLogHeader(broLogFn)
-	asserErrorThrown(t, err, "mismatched header fields")
+	assert.EqualError(t, err, "mismatched header fields")
 }
 
 func zeekHeaderDateFieldParseFail(t *testing.T) {
 	broLogFn :=  "test_input/bad_dates_header.log"
 	_, err := parseZeekLogHeader(broLogFn)
-	asserErrorThrown(t, err, "date not parsed for open field")
+	assert.EqualError(t, err,"date not parsed for open field" )
 }
 
 func TestParseZeekLogHeader(t *testing.T) {
@@ -79,30 +62,27 @@ func TestUnescapeFieldValue(t *testing.T) {
 	shouldBe := "something easy"
 
 	result := UnescapeFieldValue(input)
-	assertStringsEqual(t, result, shouldBe)
+	assert.Equal(t, result, shouldBe)
 
 	encodedInput := "\x09"
 	shouldBe =  "	"
 
 	result = UnescapeFieldValue(encodedInput)
-	assertStringsEqual(t, result, shouldBe)
+	assert.Equal(t, result, shouldBe)
 }
 
 func TestZeekLogLineToSeparator(t *testing.T) {
 	inputWithHex := "#separator \\x09"
 	sep := zeekLogLineToSeparator(inputWithHex)
-	shouldBe := "	"
-	assertStringsEqual(t, sep, shouldBe)
+	assert.Equal(t, sep, "	")
 
 	inputComma := "#separator ,"
 	sep = zeekLogLineToSeparator(inputComma)
-	shouldBe = ","
-	assertStringsEqual(t, sep, shouldBe)
+	assert.Equal(t, sep, ",")
 
 	inputShouldntBeProcessed := "hello"
 	sep = zeekLogLineToSeparator(inputShouldntBeProcessed)
-	shouldBe = ""
-	assertStringsEqual(t, sep, shouldBe)
+	assert.Equal(t, sep, "")
 }
 
 func TestZeekLogPullVar(t *testing.T) {
@@ -111,14 +91,13 @@ func TestZeekLogPullVar(t *testing.T) {
 	valueShouldBe := "value"
 
 	field, value := zeekLogPullVar(input, " ")
-	assertStringsEqual(t, field, fieldShouldBe)
-	assertStringsEqual(t, value, valueShouldBe)
+	assert.Equal(t, field, fieldShouldBe)
+	assert.Equal(t, value, valueShouldBe)
 
 	input = "dummyField value"
 	allShouldBe := ""
 
 	field, value = zeekLogPullVar(input, " ")
-
-	assertStringsEqual(t, field, allShouldBe)
-	assertStringsEqual(t, value, allShouldBe)
+	assert.Equal(t, field, allShouldBe)
+	assert.Equal(t, value, allShouldBe)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/jakubd/go-zeek-logparse
 
 go 1.13
 
-require github.com/sirupsen/logrus v1.6.0
+require (
+	github.com/sirupsen/logrus v1.6.0
+	github.com/stretchr/testify v1.2.2
+)
