be617e4d812b17d6f31779d6e4013160746a1d71
jakub <jakub.dalek@utoronto.ca>
2020-06-17 21:13:17 -0400

    some fields from dns are being parsed now

--- go-zeek-logparse/dns.go
+++ go-zeek-logparse/dns.go
@@ -5,6 +5,12 @@ and generates type cast structures specifically for dns.log parsing.
 
 package zeekparse
 
+import (
+	log "github.com/sirupsen/logrus"
+	"strconv"
+	"time"
+)
+
 // dns.log fields
 // ------------
 // defined here: https://docs.zeek.org/en/current/scripts/base/protocols/dns/main.zeek.html#type-DNS::Info
@@ -18,6 +24,7 @@ package zeekparse
 // id_resp_p:port - responders port
 // proto:enum - protocol
 // trans_id:count - identifier assigned by the program that generated the query.
+// rtt:int - round trip time for query + resp
 // query:string  - the query
 // qclass:count - QCLASS field in the question section
 // qclass_name:string - descriptive name of the QCLASS
@@ -34,10 +41,89 @@ package zeekparse
 // TTLs:vector[interval] - vector of TTL of the responses lifespan in cache
 // rejected:bool - rejected by server?
 
-func thisLogEntryToDNSStruct(givenZeekLogEntry ZeekLogEntry) (err error) {
+type Proto string
+
+const (
+	TCP Proto = "TCP"
+	UDP Proto = "UDP"
+)
+
+type DNSField struct {
+	ts         time.Time
+	uid        string
+	idOrigH    string
+	idOrigP    int
+	idRespH    string
+	idRespP    int
+	proto      Proto
+	transId    int
+	rtt        int
+	query      string
+	qclass     int
+	qclassName string
+	qtype      int
+	qtypeName  string
+	rcode      int
+	rcodeName  string
+	AA         bool
+	TC         bool
+	RD         bool
+	RA         bool
+	Z          int
+	answers    []string
+	TTLs       []int
+	rejected   bool
+}
+
+func thisLogEntryToDNSStruct(givenZeekLogEntry ZeekLogEntry) (DNSEntry DNSField, err error) {
+	for _, thisField := range givenZeekLogEntry {
+		switch thisField.fieldName {
+		case "uid":
+			DNSEntry.uid = thisField.value
+		case "id.orig_h":
+			DNSEntry.idOrigH = thisField.value
+		case "id.orig_p":
+			var convErr error
+			DNSEntry.idOrigP, convErr = strconv.Atoi(thisField.value)
+			if convErr != nil {
+				err = convErr
+				return
+			}
+		case "id.resp_h":
+			DNSEntry.idRespH = thisField.value
+		case "id.resp_p":
+			DNSEntry.idRespP, err = strconv.Atoi(thisField.value)
+			if err != nil {
+				return
+			}
+		case "proto":
+			if thisField.value == "udp" {
+				DNSEntry.proto = UDP
+			} else if thisField.value == "tcp" {
+				DNSEntry.proto = TCP
+			}
+		case "trans_id":
+			DNSEntry.transId, err = strconv.Atoi(thisField.value)
+			if err != nil {
+				return
+			}
+		case "rtt":
+			if thisField.value == "-" {
+				DNSEntry.rtt = -1
+			} else {
+				DNSEntry.rtt, err = strconv.Atoi(thisField.value)
+				if err != nil {
+					return
+				}
+			}
+		default:
+			log.Infof("unimplmented field: %s", thisField.fieldName)
+		}
+	}
 	return
 }
 
-func thisFieldToDNSStruct(givenZeekLogField ZeekLogField) (err error) {
-	return
-}
+// NOTE: bin this for now i don't think i can do this with mixed types
+//func thisFieldToDNSStruct(givenZeekLogField ZeekLogField) (err error) {
+//	return
+//}
--- go-zeek-logparse/dns_test.go
+++ go-zeek-logparse/dns_test.go
@@ -1,1 +1,24 @@
 package zeekparse
+
+import (
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestDNSParse(t *testing.T) {
+	log.SetFormatter(&log.TextFormatter{ForceColors: true})
+	log.SetLevel(log.InfoLevel)
+
+	// compressed case
+	compressedResults, compErr := parseZeekLog("test_input/simple_dns.log.gz")
+	for _, thisResult := range compressedResults {
+		fmt.Println(thisResult)
+		dnsRes, dnsErr := thisLogEntryToDNSStruct(thisResult)
+		fmt.Println(dnsRes)
+		assert.True(t, len(dnsRes.uid) >= 17 && len(dnsRes.uid) <= 18)
+		assert.NoError(t, dnsErr)
+	}
+	assert.NoError(t, compErr)
+}
